0cfaacd2ba4408bd5931b73d17f7a8898a585001
kaylane sophia <128331259+kaysophia05@users.noreply.github.com>
2025-08-27 08:31:24 -0300

    Update script.js - add coments

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-let t = 0;
-let startSeg, endSeg;
+// ============Variáveis Globáis=========================
 
+let t = 0; //Inicializando o Tempo para animar a diagonal 
+let startSeg, endSeg; // Vetores para guardar o inicio e fim do seguimento (Diagonal vermelha)
+
+// Cria uma área de desenho (canvas) de 600×600 pixels em 3D, definindo também os pontos inicial e final da diagonal.
 function setup() {
   createCanvas(600, 600, WEBGL);
-  startSeg = createVector(-100, 100, 0);
+  startSeg = createVector(-100, 100, 0); 
   endSeg   = createVector(100, -100, 0);
 }
 
+// Redesenha a cena a cada frame.
 function draw() {
   background(200); // fundo acinzentado
-  orbitControl();
+  orbitControl(); // controles de câmera com o mouse
 
   // Eixos
   push();
@@ -20,16 +24,17 @@ function draw() {
   line(0,0,-300,0,0,300);
   pop();
 
-  // Diagonal vermelha
-  stroke(255, 0, 0);
+  // ================ Diagonal vermelha (Seguimento) ===============
+  stroke(255, 0, 0); //Cor 
   strokeWeight(6); // espessura das diagonal
-  line(startSeg.x, startSeg.y, startSeg.z, endSeg.x, endSeg.y, endSeg.z);
+  line(startSeg.x, startSeg.y, startSeg.z, endSeg.x, endSeg.y, endSeg.z); // desenha do ponto fixo (startSeg) até o ponto móvel (endSeg).
 
+  // Animação da ponta (endSeg)
   let amplitude = 50;
   endSeg.x = 100 + sin(t * 2 * PI) * amplitude;
   endSeg.y = -100 - sin(t * 2 * PI) * amplitude;
 
-  // Plano
+  // Plano 
   let p1 = startSeg.copy();
   let p2 = endSeg.copy();
   let p3 = createVector(endSeg.x, endSeg.y + 200, 0);
@@ -45,14 +50,14 @@ function draw() {
   vertex(p4.x, p4.y, p4.z);
   endShape(CLOSE);
 
-  t += 0.01;
-  if(t > 1) t = 0;
+  t += 0.01; // Incrementa o tempo em 0.01 → faz o seno mudar a cada frame.
+  if(t > 1) t = 0; // Reinicia 't' quando ele passa de 1 (um ciclo completo do seno).
 
   // Marcador final
-  push();
-  noStroke();
+  push();   // Salva as configurações de estilo e transformação atuais.
+  noStroke();  // Remove contorno da esfera.
   fill(0, 255, 0);
-  translate(endSeg.x, endSeg.y, endSeg.z);
-  sphere(8); // marcador maior
-  pop();
+  translate(endSeg.x, endSeg.y, endSeg.z);  // Move o sistema de coordenadas para a ponta da diagonal (endSeg).
+  sphere(8); //  // Desenha uma esfera verde de raio 8 no ponto final.
+  pop();  // Restaura configurações anteriores (para não afetar outros desenhos).
 }
